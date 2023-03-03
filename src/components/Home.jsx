9ee3887c854917617a441af2201613e7c0fe8b32
venkatsp <venkatsp2002@gmail.com>
2023-03-03 21:59:32 +0530

    Delete option added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,53 @@
 import React, { useState } from "react";
 import '../css/Home.css';
-import notes from "../data/data.js";
+import { MdDelete, MdEdit } from 'react-icons/md'
 
 
 const Home = () => {
 
-    const[title, Settitle] = useState("");
+    const [title, Settitle] = useState("");
     const [descript, Setdescript] = useState("");
+    const [note, Setnote] = useState([]);
 
-    const handlechange = (event) =>{
+    const handlechange = (event) => {
         console.log("fun1");
         Settitle(event.target.value);
-    } 
+    }
     const handlechange1 = (event) => {
         console.log("fun2");
         Setdescript(event.target.value);
     }
 
-    function additem() {
+    const additem = () => {
         console.log("fun3");
-        notes.push(
-            {
-                "title": title,
-                "content": descript,
-                "color": generateColor()
-            }
-        );
-        console.log(notes[notes.length-1]);
+        let id;
+        if(note.length==null){
+            id = 1;
+        }
+        else{
+            id = note.length + 1;
+        }
+        const newitem = {
+            "id": id,
+            "title": title,
+            "content": descript,
+            "color": generateColor()
+        };
+        Setnote([...note, newitem]);
+    }
+
+    const removeitem = (element) => {
+        const newarr = note.filter(function (e) {
+            return e.id !== element;
+        });
+        console.log(newarr);
+        Setnote(newarr);
     }
 
     const generateColor = () => {
         const colors = ["lightblue", "#C3FDB8", "#FFFFCC", "#FFDAB9"]
         let color = "";
-        color = colors[Math.random() * (5)];
+        color = colors[Math.floor(Math.random() * (4))];
         return color;
     };
 
@@ -41,26 +56,30 @@ const Home = () => {
             <h1>Notes</h1>
             <div className="notes">
                 {
-                    notes.map((elem) => {
+                    note.map((elem) => {
                         return (
                             <div className="note" style={{ backgroundColor: `${elem.color}` }}>
-                                <h2>{elem.title}</h2>
-                                <p>{elem.content}</p>
                                 <div>
-
+                                    <h2>{elem.id}</h2>
+                                    <h2>{elem.title}</h2>
+                                    <p>{elem.content}</p>
+                                </div>
+                                <div className="row">
+                                    <MdDelete className="btn" onClick={() => removeitem(elem.id)} size={25} style={{ color: "red" }} />
+                                    <MdEdit className="btn" size={25} style={{ color: "orange" }} />
                                 </div>
                             </div>
                         )
                     }
                     )
                 }
-                <div className="add-button" onClick={() => additem()}>
+                <div className="add-button">
                     <label>Title</label>
                     <input className="title" value={title} onChange={handlechange} />
                     <label for="content">Description</label>
                     <textarea className="textarea1" value={descript} onChange={handlechange1} />
                     <div className="row">
-                        <input type="button" id="submit" className="submit" value={"Add"} />
+                        <input type="button" id="submit" className="submit" value={"Add"} onClick={additem} />
                     </div>
                 </div>
             </div>
